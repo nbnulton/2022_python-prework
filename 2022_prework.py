3661de61b7e9417b8639c67612599d1f30ad2bdf
Natasha Nulton <nbnulton@gmail.com>
2022-12-19 18:46:07 -0500

    turn in

---
+++ 2022_prework.py
@@ -0,0 +1,100 @@
+# Question 1
+# Write a function to print "hello_USERNAME!" USERNAME is the input of the function. The first line of the code has been defined as below.
+
+#     def hello_name(user_name):
+#         .....
+
+# def hello_name(user_name):
+#     message = ("hello_" + user_name)
+#     print(message)
+
+# hello_name("USERNAME")
+
+
+
+
+# Question 2
+# Write a python function, first_odds that prints the odd numbers from 1-100 and returns nothing
+
+#     def first_odds():
+#         .....
+
+
+
+# def first_odds():
+    
+#     x = range(0,101)
+
+#     num = [n for n in x if n % 2 == 1]
+
+#     print(num)
+
+# first_odds()  
+
+
+
+
+
+                
+# Question 3
+# Please write a Python function, max_num_in_list to return the max number of a given list. The first line of the code has been defined as below.
+
+#     def max_num_in_list(a_list):
+#         .....
+
+
+# def max_num_in_list(a_list):
+
+#     print(max(a_list))
+
+
+# max_num_in_list([1,2,3,246,2])
+
+
+
+
+                
+# Question 4
+# Write a function to return if the given year is a leap year. A leap year is divisible by 4, but not divisible by 100, unless it is also divisible by 400. The return should be boolean Type (true/false).
+
+#     def is_leap_year(a_year):
+#         .....
+
+
+# def is_leap_year(a_year):
+
+#     if a_year % 4 == 0:
+#         if a_year % 400 == 0:
+#             print(True)
+#         elif a_year % 100 != 0:
+#             print(False)
+#     else:
+#         print(False)
+    
+
+# is_leap_year(2000)
+
+                
+
+
+
+# Question 5
+# Write a function to check to see if all numbers in list are consecutive numbers. For example, [2,3,4,5,6,7] are consecutive numbers, but [1,2,4,5] are not consecutive numbers. The return should be boolean Type.
+
+#     def is_consecutive(a_list):
+#         .....
+
+
+# def is_consecutive(a_list):
+
+
+
+#     for i in range(a_list[0]):
+#         if i + 1 == a_list[1]:
+#             print(True)
+#         else:
+#             print(False)
+
+
+# is_consecutive([1,2,3])    
+
